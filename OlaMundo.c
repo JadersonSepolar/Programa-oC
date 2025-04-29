12c9f829043bfc4d664d0d879df41d210f670096
JadersonSepolar <jsepolar@gmail.com>
2025-04-29 22:49:04 +0000

    Operadoreslogicos_if_else_e elseif

--- OlaMundo.c
+++ OlaMundo.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+int main() {
+    int a = 10;
+    int b = 3;
+    float quociente = (double) a / b;//O valor inteiro foi transformado em um float ou double
 
+
+    printf("A soma de A+B é: %.2f\n", quociente);
+    //printf("A diferença de A-B é: %.2f\n", diferenca);
+    //printf("O produto de A*B é: %.2f\n", produto);
+    //printf("O quociente de A/B é: %.2f\n", quociente);
+
+        return 0;
+
+}
