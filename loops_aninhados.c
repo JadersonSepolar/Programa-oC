311e4377d076186d6f59e59e6b98ca8ce877c62f
JadersonSepolar <jsepolar@gmail.com>
2025-05-17 23:22:33 +0000

    loops avancados

--- loops_aninhados.c
+++ loops_aninhados.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 int main(){
 
-    for (int i = 1; i <= 10; i++){
+// variáveis utilizadas
 
-        for (int j = 1; j <= 10; j++){
-            
-            printf("%d * %d = %d\n",j, i, i * j);
+int i, j;
+char letra;
 
+    for (i = 0; i <= 3; i++){
+
+        letra = '0';
+
+        for (j = 1; j <= i; j++)
+        {
+            printf("%c ", letra);
+            ++letra;
         }
-
-    printf("\n");
+            printf("\n");
     }
-
-
+    
     return 0;
 }
---
+++ loops_declarar_avancados.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+
+    for (int i = 10, j = 0; i > j; i--, j++){
+        printf("i = %d, j = %d \n", i, j);
+    }
+
+    return 0;
+}
